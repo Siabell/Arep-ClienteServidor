2ba01fcab0b40c69ab5de205d14397dc764dded5
Siabell <valesiabatto@gmail.com>
2020-02-06 04:27:12 -0500

    reto 1 sin excepciones

--- src/main/java/edu/escuelaing/arep/reto1/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/reto1/HttpServer.java
@@ -1,12 +1,17 @@
 package edu.escuelaing.arep.reto1;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.Date;
 import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 public class HttpServer {
 
-	static final File WEB_ROOT = new File(".");
+	static final File WEB_ROOT = new File(System.getProperty("user.dir") + "/src/main/resources");
 	static final String DEFAULT_FILE = "index.html";
     static final String FILE_NOT_FOUND = "404.html";
     static final int PORT = 36000;
@@ -26,7 +31,7 @@ public class HttpServer {
     	 PrintWriter out =null;
     	 BufferedReader in =null;
     	 BufferedOutputStream outputLine = null;
-    	 String fileR = null;
+    	 String fileReq = null;
     	 try {
     		 while (true) {
     			 clientSocket = serverSocket.accept();
@@ -42,15 +47,46 @@ public class HttpServer {
     			 StringTokenizer header = new StringTokenizer(inputLine);
     			 String method = header.nextToken().toUpperCase();
     			 //http method
-    			 fileR = header.nextToken().toLowerCase();
+    			 fileReq = header.nextToken().toLowerCase();
     			 
     			 //check Method
-    			 if (!method.equals("GET")) {
-    				 String contentMimeType = "text/html";
+    			 if (method.equals("GET")) {
+    				 /*
+    				 System.out.println("method");
+    				 File file = new File(WEB_ROOT,DEFAULT_FILE);
+    				 sendHeader(out,file,"text/html",outputLine,"200 ok");*/
+    				 if (fileReq.endsWith("/")) {
+    					 File file = new File(WEB_ROOT,DEFAULT_FILE);
+		 				 sendResponse(out,file,"text/html",outputLine,"200 ok");
+		 			}else {
+		 				File file = new File(WEB_ROOT,fileReq);
+		 				if (!file.exists()) {
+		 					//impelentar
+		 				} else {
+		 					
+		 					String contentMimeType = defineContentType(fileReq);
+		 					//implementar si no se encuentra
+		 					sendResponse(out,file,contentMimeType,outputLine,"200 ok");
+		 				}
+		 			}
+    			 } else {
+    				 //metodo no permitido
+    				 if (fileReq.endsWith("/")) {
+    						fileReq += DEFAULT_FILE;
+    					}
+    				 File file = new File(WEB_ROOT,DEFAULT_FILE);
+    				 sendResponse(out,file,"text/html",outputLine,"200 ok");
     			 }
     		 }
 		} catch (Exception e) {
 			// TODO: handle exception
+			System.err.println("Server error : " + e);
+		} finally {
+			out.close();
+			in.close();
+			outputLine.close();
+			clientSocket.close();
+			serverSocket.close();
 		}
     
     }
@@ -70,22 +106,59 @@ public class HttpServer {
 		return fileData;
 	}
     
-    private static void sendHeader (PrintWriter out,File file, String contentMimeType,BufferedOutputStream outputLine, String answer ) {
+    private static void sendResponse (PrintWriter out,File file, String contentType,BufferedOutputStream outputLine, String answer ) {
     	out.println("HTTP/1.1 "+ answer);
 		out.println("Server: Java HTTP Server  : 1.0");
 		out.println("Date: " + new Date());
-		out.println("Content-type: " + contentMimeType);
+		out.println("Content-type: " + contentType);
 		out.println("Content-length: " + file.length());
-		out.println(); // blank line between headers and content, very important !
+		out.println(); // blank line between headers and content,
 		out.flush(); // flush character output stream buffer
 		// file
 		try {
-			outputLine.write(fileDataByte(file), 0, (int) file.length());
-			outputLine.flush();
+			String[] type = contentType.split("/");
+			System.out.println(contentType);
+			if (contentType.equals("image/jpg") && contentType.equals("image/png") ) {
+				BufferedImage image = ImageIO.read(file);
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		        ImageIO.write(image, type[1], byteArrayOutputStream);
+		        byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+		        outputLine.write(size);
+		        outputLine.write(byteArrayOutputStream.toByteArray());
+		        outputLine.flush();
+			}else {
+				System.out.println("texto");
+				outputLine.write(fileDataByte(file), 0, (int) file.length());
+				outputLine.flush();
+			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
     }
+ 
+    private static String defineContentType(String fileReq ) {
+    	String answer = null;
+    	if (fileReq.endsWith(".htm")  ) {
+    		answer = "text/html";
+    	} else if (fileReq.endsWith(".html")  ) {
+    		answer = "text/html";
+    	} else if (fileReq.endsWith(".jpg") ) {
+    		answer = "image/jpg";
+    	} else if (fileReq.endsWith(".png") ) {
+    		answer = "image/png";
+    	}
+    	else {
+    		answer = "text/plain";
+    	}
+    	System.out.println("la respuesta es :  "+fileReq);
+    	System.out.println("la termina es :  "+fileReq.endsWith(".html"));
+    	System.out.println("la respuesta es :  "+answer);
+    	return answer;
+    }
+    
+    
     
 }
